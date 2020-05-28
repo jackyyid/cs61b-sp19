42b862f685f416ad6fb929349a1316644cb768d5
jacky <jackyqu828@gmail.com>
2020-05-27 23:17:23 -0700

    Minor change on Lab 1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -7,7 +7,7 @@ public class LeapYear {
     /** Return whether a year is a leap year. */
     public static boolean isLeapYear(int year) {
         if (year % 400 == 0) return true;
-        else if (year % 4 == 0 && year % 100 != 0) return true;
+        if (year % 4 == 0 && year % 100 != 0) return true;
         return false;
     }
 
